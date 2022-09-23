097ebe86383606f89b217d5b2410c65ca579926c
Deepak Joshi <deepakjoshi982001@gmail.com>
2022-09-23 14:59:43 +0530

    complete o cart error

--- src/DumyList.jsx
+++ src/DumyList.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import DummyMain from "./DummyMain";
+import Loading from "./Loading";
 function DumyList({ setCartProduct, ProductTotalCount, cartProduct }) {
+  console.log(cartProduct.length);
   return (
     <>
-      <div>
-        {cartProduct.map(function (main) {
-          return (
-            <DummyMain
-              ProductTotalCount={ProductTotalCount}
-              title={main.title}
-              category={main.category}
-              photo={main.thumbnail}
-              price={main.price}
-              rating={main.rating}
-              sale={main.sale}
-              {...main}
-            />
-          );
-        })}
-      </div>
+      {cartProduct.length <= 0 ? (
+        <div className="flex items-center justify-center h-screen text-4xl font-black text-blue-600 bg-blue-200">
+          Please Add Something To Cart First........
+        </div>
+      ) : (
+        <div>
+          {cartProduct.map(function (main) {
+            return (
+              <DummyMain
+                ProductTotalCount={ProductTotalCount}
+                title={main.title}
+                category={main.category}
+                photo={main.thumbnail}
+                price={main.price}
+                rating={main.rating}
+                sale={main.sale}
+                {...main}
+              />
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
