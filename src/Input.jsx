9aef86e13a24dd29d73744884ffb8e223052da88
DeepakJoshi100 <113166656+DeepakJoshi100@users.noreply.github.com>
2022-09-28 21:12:45 +0530

    Revert "Addingdelete"

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,16 @@
+import autoprefixer from "autoprefixer";
+import { useField } from "formik";
 import React from "react";
-function Input({ label, id, name, className, touched, error, ...rest }) {
+function Input({ label, id, name, className, ...rest }) {
+  const field = useField(name);
+  const [data, meta] = field;
+  const { value, onBlur, onChange } = data;
+  const { error, touched } = meta;
   let borderClass = "";
   if (error && touched) {
-    borderClass = "border-red-500 focus:text-red-500 focus:border-red-400 ";
+    borderClass = "border-red-400 focus:text-red-500 focus:border-red-400 ";
   }
+
   return (
     <>
       <div className="flex flex-col">
@@ -13,16 +20,20 @@ function Input({ label, id, name, className, touched, error, ...rest }) {
         <input
           id={id}
           name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
           {...rest}
           className={
             "rounded-md px-2 py-1 border-2 border-gray-300 focus:z-10 focus:border-2 focus:border-blue-600 focus:outline-none focus:ring-blue-600 " +
-            borderClass +
-            className
+            className +
+            " " +
+            borderClass
           }
         />
         {touched && error && (
-          <div className="text-xs font-black text-red-500">{error}</div>
-        )}
+          <div className="text-red-500 text-xs font-black">{error}</div>
+        )}{" "}
       </div>
     </>
   );
